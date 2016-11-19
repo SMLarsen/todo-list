9d4e5b766d7c33368d255be1f1780edd91b85e7a
Steve Larsen <stevelarsen01@gmail.com>
2016-11-19 10:36:17 -0600

    add delete confirmation dialog

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,119 +4,142 @@ $(document).ready(function() {
     addDatePicker();
     getTodo();
 
-//=====================  Event Listeners  ===============================
-    $('#addButton').on('click', function (event){
-      event.preventDefault();
-      console.log($(this));
-      addTodo();
-      getTodo();
+    //=====================  Event Listeners  ===============================
+    $('#addButton').on('click', function(event) {
+        event.preventDefault();
+        console.log($(this));
+        addTodo();
+        getTodo();
     });
 
-    $('#todoList').on('click','.completeButton', function (){
-      var todoId = $(this).parent().data('id');
-      console.log('Request to complete:', todoId);
-      completeTodo();
+    $('#todoList').on('click', '.completeButton', function() {
+        var todoId = $(this).parent().data('id');
+        console.log('Request to complete:', todoId);
+        completeTodo();
     });
 
-    $('#todoList').on('click', '.deleteButton', function (){
-      var todoId = $(this).parent().data('id');
-      console.log('Request to delete:', todoId);
-      deleteTodo(todoId);
+    $('#todoList').on('click', '.deleteButton', function() {
+        var todoId = $(this).parent().data('id');
+        console.log('Request to delete:', todoId);
+        deleteConfirm(todoId);
     });
 
-//  =======================  Functions  ==================================
+    //  =======================  Functions  ==================================
 
-// add a todo
+    // add a todo
     function addTodo() {
 
-      var todo = {};
+        var todo = {};
 
-      $.each($('.addTodo').serializeArray(), function(i, field) {
-          todo[field.name] = field.value;
-      });
-      console.log(todo);
-      $.ajax({
-          type: 'POST',
-          url: '/todo',
-          data: todo,
-          success: function(data) {
-            console.log("Success - POST /todo");
-          },
-          error: function(){
-            console.log("Error - POST /todo");
-          }
-      });
-    }  // end function addTodo
+        $.each($('.addTodo').serializeArray(), function(i, field) {
+            todo[field.name] = field.value;
+        });
+        console.log(todo);
+        $.ajax({
+            type: 'POST',
+            url: '/todo',
+            data: todo,
+            success: function(data) {
+                console.log("Success - POST /todo");
+            },
+            error: function() {
+                console.log("Error - POST /todo");
+            }
+        });
+    } // end function addTodo
 
-// delete a todo
+    // delete a todo
     function deleteTodo(todoId) {
-          $.ajax({
-              type: 'DELETE',
-              url: '/todo/delete/' + todoId,
-              success: function(result) {
+        $.ajax({
+            type: 'DELETE',
+            url: '/todo/delete/' + todoId,
+            success: function(result) {
                 getTodo();
-              },
-              error: function(result) {
-                  console.log('Unable to delete todo:', todoId);
-              }
-          });
+            },
+            error: function(result) {
+                console.log('Unable to delete todo:', todoId);
+            }
+        });
     } // end function deleteTodo
 
     // complete a todo
     function completeTodo(todoId) {
-          $.ajax({
-              type: 'PUT',
-              url: '/todo/complete/' + todoId,
-              success: function(result) {
+        $.ajax({
+            type: 'PUT',
+            url: '/todo/complete/' + todoId,
+            success: function(result) {
                 getTodo();
-              },
-              error: function(result) {
-                  console.log('Unable to complete todo:', todoId);
-              }
-          });
+            },
+            error: function(result) {
+                console.log('Unable to complete todo:', todoId);
+            }
+        });
     } // end function completeTodo
 
-// get all todos
+    // get all todos
     function getTodo() {
-      $.ajax({
-          type: 'GET',
-          url: '/todo',
-          success: function(data) {
-            // displayResult(data);
-            console.log("Success - GET /");
-            buildTodoList(data);
-          },
-          error: function(){
-            console.log("Error - GET /");
-          }
-      });
-    }  // end function getTodo
+        $.ajax({
+            type: 'GET',
+            url: '/todo',
+            success: function(data) {
+                // displayResult(data);
+                console.log("Success - GET /");
+                buildTodoList(data);
+            },
+            error: function() {
+                console.log("Error - GET /");
+            }
+        });
+    } // end function getTodo
 
-// build the todo list
+    // build the todo list
     function buildTodoList(todos) {
-      $("#todoList").empty();
-      for (var i = 0; i < todos.length; i++) {
-        todo = todos[i];
-        // console.log(todo);
-        var string = '';
-        if (todo.status_name === 'Closed') {
-          string += '<div class="todo closedTodo"';
-        } else {
-          string += '<div class="todo"';
+        $("#todoList").empty();
+        for (var i = 0; i < todos.length; i++) {
+            todo = todos[i];
+            // console.log(todo);
+            var string = '';
+            if (todo.status_name === 'Closed') {
+                string += '<div class="todo closedTodo"';
+            } else {
+                string += '<div class="todo"';
+            }
+            string += ' data-id=' + todo.id + '>';
+            string += '<h2>' + todo.description + '</h2><p>Date Due: <span>' + todo.due_date + '</span>, Status: <span>' + todo.status_name + '</span></p>';
+            string += '<button type="button" class="completeButton" name="completeButton">Complete</button>';
+            string += '<button type="button" class="deleteButton" name="deleteButton">Delete</button></div>';
+            $("#todoList").append(string);
+
+            // console.log('id: ', $("#todoList").children().last().data('id'));
         }
-        string += ' data-id=' + todo.id + '>';
-        string += '<h2>' + todo.description + '</h2><p>Date Due: <span>' + todo.due_date + '</span>, Status: <span>' + todo.status_name + '</span></p>';
-        string += '<button type="button" class="completeButton" name="completeButton">Complete</button>';
-        string += '<button type="button" class="deleteButton" name="deleteButton">Delete</button></div>';
-        $("#todoList").append(string);
+    } // end function buildTodoList
 
-        // console.log('id: ', $("#todoList").children().last().data('id'));
-      }
-    }  // end function buildTodoList
-
-// add jquery-ui datepicker to DOM
+    // add jquery-ui datepicker to DOM
     function addDatePicker() {
-      $("#dueDate").datepicker();
+        $("#dueDate").datepicker();
     } // end function addDatePicker
+
+
+    // add jquery-ui dialog for delete confirmation to DOM
+    function deleteConfirm(todoId) {
+        $(function() {
+            $("#dialog-confirm").dialog({
+                resizable: false,
+                height: "auto",
+                width: 400,
+                modal: true,
+                dialogClass: "no-close",
+                buttons: {
+                    "Delete Task": function() {
+                        deleteTodo(todoId);
+                        $(this).dialog("close");
+                    },
+                    Cancel: function() {
+                        $(this).dialog("close");
+                    }
+                }
+            });
+        });
+    } // end function deleteConfirm
 
 });
